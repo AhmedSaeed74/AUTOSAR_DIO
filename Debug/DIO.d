33b6a1cd4ca317daca135e9ec7297b415b42f448
AhmedSaeed74 <ahmedsaeed9674@gmail.com>
2023-10-07 22:39:05 +0300

    first commit

---
+++ Debug/DIO.d
@@ -0,0 +1,7 @@
+DIO.o: ../DIO.c ../tm4c123gh6pm.h ../Dio.h ../Std_Types.h
+
+../tm4c123gh6pm.h:
+
+../Dio.h:
+
+../Std_Types.h:
---
+++ Debug/Port.d
@@ -0,0 +1,7 @@
+Port.o: ../Port.c ../tm4c123gh6pm.h ../Port.h ../Std_Types.h
+
+../tm4c123gh6pm.h:
+
+../Port.h:
+
+../Std_Types.h:
